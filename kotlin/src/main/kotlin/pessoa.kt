6046e87fc929ee01ad9e729b159a7a614df961d2
EliasLobobm <eliaslobbo@gmail.com>
2021-03-09 22:33:54 -0300

    enviando o estudo da private e override da classe

--- kotlin/src/main/kotlin/pessoa.kt
+++ kotlin/src/main/kotlin/pessoa.kt
@@ -1,10 +1,13 @@
 class pessoa {
     var nome:String = "Elias"
     var cpf:String = "009632444705"
+    private set
 }
 
 fun main(){
     var elias = pessoa()
+    println(elias)
+    //elias.cpf = "0731402790"
     println(elias.nome)
     println(elias.cpf)
 }
